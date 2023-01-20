0c4847e139e80e1a4941a9b47590f3da9e53633b
Tanveer <tanveerdalal11@gmail.com>
2023-01-20 00:21:38 +0000

    Data Science Portfolio

---
+++ data-science-portfolio/src/components/Projects.js
@@ -0,0 +1,103 @@
+import { Container, Row, Col, Tab, Nav, Form } from "react-bootstrap";
+import { ProjectCard } from "./ProjectCard";
+import project1 from "../assets/img/project-1.png"
+import project2 from "../assets/img/project-2.png"
+import projImg1 from "../assets/img/project-img1.png";
+import projImg2 from "../assets/img/project-img2.png";
+import projImg3 from "../assets/img/project-img3.png";
+import colorSharp2 from "../assets/img/color-sharp2.png";
+import 'animate.css';
+import TrackVisibility from 'react-on-screen';
+
+export const Projects = () => {
+
+  const projects = [
+    {
+      title: "Parking Space Finder",
+      description: "Real Time information about the number of available parking space",
+      imgUrl: project1,
+    },
+    {
+      title: "American Sign Language Recognition",
+      description: "User will make some gestures then the screen will display alphabets and numeric values",
+      imgUrl: project2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg1,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg2,
+    },
+    {
+      title: "Business Startup",
+      description: "Design & Development",
+      imgUrl: projImg3,
+    },
+  ];
+
+  return (
+    <section className="project" id="project">
+      <Container>
+        <Row>
+          <Col size={12}>
+            <TrackVisibility>
+              {({ isVisible }) =>
+              <div className={isVisible ? "animate__animated animate__fadeIn": ""}>
+                <h2>Projects</h2>
+                <p>
+                  I am still learning in the field of AI/ML Engineering so there are less projects that are build and can be found on my GitHub.<br></br>
+                  I strongly belived that you and I can build more interactive projects together if you provide me a chance to prove myself.
+                </p>
+                <Tab.Container id="projects-tabs" defaultActiveKey="first">
+                  <Nav variant="pills" className="nav-pills mb-5 justify-content-center align-items-center" id="pills-tab">
+                    <Nav.Item>
+                      <Nav.Link eventKey="first">Tab 1</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="second">Tab 2</Nav.Link>
+                    </Nav.Item>
+                    <Nav.Item>
+                      <Nav.Link eventKey="third">Tab 3</Nav.Link>
+                    </Nav.Item>
+                  </Nav>
+                  <Tab.Content id="slideInUp" className={isVisible ? "animate__animated animate__slideInUp" : ""}>
+                    <Tab.Pane eventKey="first">
+                      <Row>
+                        {
+                          projects.map((project, index) => {
+                            return (
+                              <ProjectCard
+                                key={index}
+                                {...project}
+                                />
+                            )
+                          })
+                        }
+                      </Row>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="second">
+                      <p>Sorry! for any inconvenience most of my personal projects are still in working progress and soon be uploaded on my web page.</p>
+                    </Tab.Pane>
+                    <Tab.Pane eventKey="third">
+                      <p>Sorry! for any inconvenience most of my personal projects are still in working progress and soon be uploaded on my web page.</p>
+                    </Tab.Pane>
+                  </Tab.Content>
+                </Tab.Container>
+              </div>}
+            </TrackVisibility>
+          </Col>
+        </Row>
+      </Container>
+      <img className="background-image-right" src={colorSharp2}></img>
+    </section>
+  )
+}
